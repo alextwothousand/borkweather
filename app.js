8d2e758f940b2d9c6b8f606f5097d262f00d358e
bork <alexrahali3@gmail.com>
2020-03-18 10:17:26 +0000

    fahrenheit update

--- app.js
+++ app.js
@@ -2,12 +2,17 @@ $(document).ready(() => {
 	const apiKey = "3dffcc381c8dd365908c495d0aa7445d";
 	let position = {};
 
-	const loadWeatherInformation = (data) => {
+	const loadWeatherInformation = (data, imperial = false) => {
 		$('#icon').attr("src", `https://openweathermap.org/img/wn/${data.weather[0].icon}@2x.png`);
 		$('#temp').text(`${Math.floor(data.main.temp)}`);
 		$('#description').text(`${data.weather[0].description}`);
 		$('#location').text(`${data.name}, ${data.sys.country} (${data.coord.lon}, ${data.coord.lat})`);
+
+		imperial === true ? $('#unit').text("F") : $('#unit').text("C");
 	};
+
+	let unit = Cookies.get("unit");
+	if (unit === undefined || unit !== "metric" || unit !== "imperial") Cookies.set("unit", "metric", { expires: 28 });
 
 	navigator.geolocation.getCurrentPosition((pos) => {
 		position.latitude = pos.coords.latitude;
@@ -15,8 +20,26 @@ $(document).ready(() => {
 
 		console.log("Able to proceed, coordinates were retrieved!");
 
-		$.get(`https://api.openweathermap.org/data/2.5/weather?lat=${position.latitude}&lon=${position.longitude}&appid=${apiKey}&units=metric`, function(data) {
+		$.get(`https://api.openweathermap.org/data/2.5/weather?lat=${position.latitude}&lon=${position.longitude}&appid=${apiKey}&units=${unit}`, function(data) {
 			loadWeatherInformation(data);
+			
+			$("#clickable").on("click", function (e) {
+				e.preventDefault();
+
+				if (unit === "metric") {
+					Cookies.set("unit", "imperial", { expires: 28 });
+					unit = "imperial";
+					$.get(`https://api.openweathermap.org/data/2.5/weather?lat=${position.latitude}&lon=${position.longitude}&appid=${apiKey}&units=imperial`, function(data) {
+						loadWeatherInformation(data, true);
+					});
+				} else {
+					Cookies.set("unit", "metric", { expires: 28 });
+					unit = "metric";
+					$.get(`https://api.openweathermap.org/data/2.5/weather?lat=${position.latitude}&lon=${position.longitude}&appid=${apiKey}&units=metric`, function(data) {
+						loadWeatherInformation(data);
+					});
+				}
+			});
 		});
 	}, 
 	() => {
