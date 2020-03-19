4bb4d3ec04a92367528133cb9f619e7290c91b12
bork <38814044+infin1tyy@users.noreply.github.com>
2020-03-19 20:36:08 +0000

    Revert "Removed jQuery and made everything Vanilla JS."

--- app.js
+++ app.js
@@ -1,37 +1,43 @@
-window.onload = () => {
+$(document).ready(() => {
 	const apiKey = "3dffcc381c8dd365908c495d0aa7445d";
 	let position = {};
 
-	const loadWeatherInformation = (data, imperial = 0) => {
-		document.getElementById('icon').setAttribute('src', `https://openweathermap.org/img/wn/${data.weather[0].icon}@2x.png`);
-		document.getElementById('temp').innerText = `${Math.floor(data.main.temp)}`;
-		document.getElementById('description').innerText = `${data.weather[0].description}`;
-		document.getElementById('location').innerText = `${data.name}, ${data.sys.country} (${data.coord.lon}, ${data.coord.lat})`;
-		document.getElementById('unit').innerText = imperial === 1 ? "F" : "C";
+	const loadWeatherInformation = (data, imperial = false) => {
+		$('#icon').attr("src", `https://openweathermap.org/img/wn/${data.weather[0].icon}@2x.png`);
+		$('#temp').text(`${Math.floor(data.main.temp)}`);
+		$('#description').text(`${data.weather[0].description}`);
+		$('#location').text(`${data.name}, ${data.sys.country} (${data.coord.lon}, ${data.coord.lat})`);
+
+		imperial === true ? $('#unit').text("F") : $('#unit').text("C");
 	};
 
 	let unit = Cookies.get("unit");
 	if (unit === undefined || unit !== "metric" || unit !== "imperial") Cookies.set("unit", "metric", { expires: 28 });
+
 	navigator.geolocation.getCurrentPosition((pos) => {
 		position.latitude = pos.coords.latitude;
 		position.longitude = pos.coords.longitude;
 
 		console.log("Able to proceed, coordinates were retrieved!");
-		makeRequest(`https://api.openweathermap.org/data/2.5/weather?lat=${position.latitude}&lon=${position.longitude}&appid=${apiKey}&units=${unit}`).then(data => {
-			let initialConvertion = unit === "imperial" ? 1 : 0;
-			loadWeatherInformation(data, initialConvertion);
-			document.getElementById('clickable').addEventListener('click', (e) => {
+
+		$.get(`https://api.openweathermap.org/data/2.5/weather?lat=${position.latitude}&lon=${position.longitude}&appid=${apiKey}&units=${unit}`, function(data) {
+			loadWeatherInformation(data);
+			
+			$("#clickable").on("click", function (e) {
 				e.preventDefault();
+
 				if (unit === "metric") {
 					Cookies.set("unit", "imperial", { expires: 28 });
 					unit = "imperial";
-					data.main.temp = doConversion(data.main.temp, 1);
-					loadWeatherInformation(data, 1);
+					$.get(`https://api.openweathermap.org/data/2.5/weather?lat=${position.latitude}&lon=${position.longitude}&appid=${apiKey}&units=imperial`, function(data) {
+						loadWeatherInformation(data, true);
+					});
 				} else {
 					Cookies.set("unit", "metric", { expires: 28 });
 					unit = "metric";
-					data.main.temp = doConversion(data.main.temp, 0);
-					loadWeatherInformation(data);
+					$.get(`https://api.openweathermap.org/data/2.5/weather?lat=${position.latitude}&lon=${position.longitude}&appid=${apiKey}&units=metric`, function(data) {
+						loadWeatherInformation(data);
+					});
 				}
 			});
 		});
@@ -39,20 +45,4 @@ window.onload = () => {
 	() => {
 		console.log("Unable to proceed, coordinates were not able to be retrieved!");
 	});
-};
-
-function makeRequest(uri) {
-	return new Promise((resolve, reject) => {
-		fetch(uri).then(response => {
-			return response.json();
-		}).then(data => {
-			resolve(data);
-		}).catch(err => {
-			reject(err);
-		});
-	});
-}
-
-function doConversion(obj, tempType) {
-	return tempType == 0 ? ((obj - 32) * 5) / 9 : ((obj / 5) * 9) + 32;
-}
+});
